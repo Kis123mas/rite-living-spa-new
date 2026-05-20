d049c3b8ae7b985a70f1368160ec184331414ed0
Kis123mas <emmanuelezekielsolomon@gmail.com>
2026-05-20 20:01:53 +0100

    working

--- spabusmgt/settings.py
+++ spabusmgt/settings.py
@@ -136,23 +136,21 @@ WSGI_APPLICATION = 'spabusmgt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+import dj_database_url
 
-DATABASES = {
-    'default' : {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kjyivvuv_ritelivingspadb',
-        'USER': 'kjyivvuv_ritelivingspadbuser',
-        'PASSWORD': 'ritelivingspa123RITELIVINGSPA',
-        'HOST': 'localhost',
-        'PORT': '5432',
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
